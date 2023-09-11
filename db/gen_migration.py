90155ba087c335f7279e8f6bf3607c735b1385ce
ezsx <79762324+ezsx@users.noreply.github.com>
2023-09-11 08:41:02 +0300

    generate models with sqlacodegen

--- db/gen_migration.py
+++ db/gen_migration.py
@@ -3,27 +3,32 @@ from sqlalchemy import MetaData, Table, Index
 from alembic import autogenerate
 from alembic.operations import ops
 
-#sqlacodegen postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres >33.txt
+# Auto generating models:
+# sqlacodegen postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres >33.txt
 
-#e = create_engine("mysql://scott:tiger@localhost/test")
-e = create_engine("postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres")
 
-with e.connect() as conn:
-    #cursor = conn._dbapi_connection.cursor()
-    #cursor.execute("SET SESSION search_path='%s'" % 'mrp')
-    m = MetaData()
-    user_table = Table('pillows', m, autoload_with=conn)
-    user_table = Table('pillows_history', m, autoload_with=conn)
-    #Index('mms_action_curr_tbl_id_export_prcn_idx',m,autoload_with=conn)
+# Auto generating models with alembicc:
 
-print(autogenerate.render_python_code(
-    ops.UpgradeOps(
-        ops=[
-            ops.CreateTableOp.from_table(table) for table in m.tables.values()
-        ] + [
-            ops.CreateIndexOp.from_index(idx) for table in m.tables.values()
-            for idx in table.indexes
-        ]
-    ))
-)
+# e = create_engine("mysql://scott:tiger@localhost/test")
 
+# e = create_engine("postgresql+psycopg2://postgres:mysecretpassword@localhost:5432/postgres")
+#
+# with e.connect() as conn:
+#     #cursor = conn._dbapi_connection.cursor()
+#     #cursor.execute("SET SESSION search_path='%s'" % 'mrp')
+#     m = MetaData()
+#     user_table = Table('pillows', m, autoload_with=conn)
+#     user_table = Table('pillows_history', m, autoload_with=conn)
+#     #Index('mms_action_curr_tbl_id_export_prcn_idx',m,autoload_with=conn)
+#
+# print(autogenerate.render_python_code(
+#     ops.UpgradeOps(
+#         ops=[
+#             ops.CreateTableOp.from_table(table) for table in m.tables.values()
+#         ] + [
+#             ops.CreateIndexOp.from_index(idx) for table in m.tables.values()
+#             for idx in table.indexes
+#         ]
+#     ))
+# )
+#
